52a9bd1f928861592a13e804eb9fa1cdcff97244
Pasbatron <131593509+pasbatron@users.noreply.github.com>
2023-11-02 11:31:45 +0700

    Update and rename run.py to run_main.py

--- run_main.py
+++ run_main.py
@@ -15,20 +15,18 @@ ser_lora = serial.Serial(
 
 condition = 1
 
-
-
 while True:
     data_serial = ser_lora.readline().decode("ascii")
     data_serial_split = data_serial.split(",")
     print("Program Running!!!")
-    time.sleep(1)
-    
-    if len(data_serial_split) >= 6:
+    time.sleep(0.3)
+    if len(data_serial_split) >=1:
         if(data_serial_split[0] == "on_good"):
-            cap = cv2.VideoCapture(0)
+            # global file_name
+            print(data_serial_split[0])
+            cap = cv2.VideoCapture(1)
             if not cap.isOpened():
                 print("Kamera tidak dapat diakses.")
-
             while condition < 2:
                 ret, frame = cap.read()
                 if not ret:
@@ -36,42 +34,27 @@ while True:
                     break
                 frame = cv2.resize(frame, (1920, 1080))
                 timestamp = time.strftime("%Y%m%d_%H%M%S")
+                
                 file_name = f"D:\on\Project_PT_Otics_Image_Processing_HLA\Version 1.1\data_trial\images\Adjustercaptured_image_{timestamp}.png"
                 cv2.imwrite(file_name, frame)
                 print("Gambar berhasil disimpan:", file_name)
                 condition += 1
-            cap.release()
-
+                cap.release()
             model = YOLO("D:\\on\Project_PT_Otics_Image_Processing_HLA\\Version 1.1\\best.pt")
-            model.predict(task="detect", mode="predict", show=True, conf=0.94, source=file_name, save_txt=True)
-            results = model.predict(task="detect", mode="predict", show=True, conf=0.94, source=file_name, save_txt=True)
 
+
+            results = model.predict(task="detect", mode="predict", conf=0.8, source=file_name, hide_labels=False, save=True)
             for r in results:
                 print(r.boxes.shape[0])
                 print(type(r.boxes.shape[0]))
-                kondisi = r.boxes.shape[0]
-                if(kondisi == 192):
-                    ser_lora.write(b",on_speaker,12,12")
-                    print("Data Terkirim")
+                condition = r.boxes.shape[0]
+                if(condition == 192):
+                    ser_lora.write(b"*oke_andon,12,12,4,4#")
                     time.sleep(1)
-                    #call(["python", "D:\\on\\Project_PT_Otics_Image_Processing_HLA\\Version 1.1\\run.py"])
                 else:
-                    ser_lora.write(b",off_speaker,12,12")
-                    print("GAGAL")
+                    ser_lora.write(b"*ng_andon,12,12,4,4#")
                     time.sleep(1)
-                    #call(["python", "D:\\on\\Project_PT_Otics_Image_Processing_HLA\\Version 1.1\\run.py"])
-
-
-
-
-
-
-
-
-
-
-
-
+                
 '''
 import cv2
 import os
@@ -82,8 +65,8 @@ def capture_image():
     if not cap.isOpened():
         print("Kamera tidak dapat diakses.")
         return
-    kondisi = 1
-    while kondisi < 2:
+    condition = 1
+    while condition < 2:
         ret, frame = cap.read()
         if not ret:
             print("Gagal membaca frame.")
@@ -93,7 +76,7 @@ def capture_image():
         file_name = f"D:\on\Project_PT_Otics_Image_Processing_HLA\Version 1.1\data_trial\images\Adjustercaptured_image_{timestamp}.png"
         cv2.imwrite(file_name, frame)
         print("Gambar berhasil disimpan:", file_name)
-        kondisi += 1
+        condition += 1
     cap.release()
 
 if __name__ == "__main__":
